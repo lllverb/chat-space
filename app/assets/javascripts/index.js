7a73ef99b7e42a05268c00b26ca0c87378e41fbd
lllverb <wktkm9orz2@gmail.com>
2019-09-11 20:20:04 +0900

    incremental-search before code review

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -13,7 +13,6 @@ $(document).on("turbolinks:load", function() {
   function appendErrMsgToHTML(msg) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${msg}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
                 </div>`
     searchResult.append(html);
   }
