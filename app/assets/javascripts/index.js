c08ee4fac66584e9710a8ca64378943e6dc6fab8
lllverb <wktkm9orz2@gmail.com>
2019-09-11 16:24:53 +0900

    get #user-search-field val

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("turbolinks:load", function() {
+  $('#user-search-field').on('keyup', function(){
+    var input = $('#user-search-field').val();
+    console.log(input)
+  });
+});
