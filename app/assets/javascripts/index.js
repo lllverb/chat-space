ec55109206868fc1560cc5c08e1c6c439a5040b1
lllverb <wktkm9orz2@gmail.com>
2019-09-11 19:56:59 +0900

    incremental-search

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,25 +1,38 @@
-document.addEventListener("turbolinks:load", function() {
-  $('#user-search-field').on('keyup', function(){
-    var input = $('#user-search-field').val();
-    console.log(input)
+$(document).on("turbolinks:load", function() {
+  var inputForm = $("#user-search-field")
+  var searchResult = $('#user-search-result');
 
+  function buildHTML(data) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${data.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                </div>`
+    searchResult.append(html);
+  };
+
+  inputForm.on('keyup', function(){
+    var target = inputForm.val();
+    console.log(target)
     $.ajax({
       type: 'GET',
-      url: '/users/index/',
-      data: {keyword: input},
+      url: '/users',
+      data: {keyword: target},
       dataType: 'json'
     })
+    .done(function(data){
 
-    .done(function(users){
-      $('#user-search-result')
-      if (users.length !== 0){
-        users.forEach(function(user){
-          appendUser(user);
+      searchResult.empty();
+      if (data.length !== 0){
+        data.forEach(function(data){
+          buildHTML(data);
         });
       }
-      else {
-        appendErrMsgToHTML("一致するユーザーはいません")
-      }
     })
+      // else {
+      //   ("一致するユーザーはいません")
+      // }
+    // .fail(function(){
+    //   alert('検索に失敗しました')
+    // })
   });
 });
