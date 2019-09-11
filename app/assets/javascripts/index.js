9fe10dcde94449bd21a23a1f95425402c2f5cb83
lllverb <wktkm9orz2@gmail.com>
2019-09-11 16:33:11 +0900

    create ajax & done method

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,5 +2,24 @@ document.addEventListener("turbolinks:load", function() {
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
     console.log(input)
+
+    $.ajax({
+      type: 'GET',
+      url: '/users/index/',
+      data: {keyword: input},
+      dataType: 'json'
+    })
+
+    .done(function(users){
+      $('#user-search-result')
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーはいません")
+      }
+    })
   });
 });
